6cdb75a5ab2baa3327efa0ee4e0da1500c585d64
shi12321 <767388689@qq.com>
2017-06-15 09:31:23 +0800

    购物车小调

--- src/js/car.js
+++ src/js/car.js
@@ -160,16 +160,14 @@ require(['config'],function(){
 		// 删除单个商品
 		biao.on('click','.shan',function(){
 
-			var tr = $(this).parent().parent();
-
 			var qian = $(this).parent().prev().find('strong');
 
 			var tr = $(this).parent().parent();
 
 			var id = tr.attr('id');
 
-			var zong = $(this).closest('div').next().find('.zong').html();
-
+			var zong = $(this).closest('.car_z_02').next().find('.zong').html();
+console.log($(this).closest('.car_z_02').next().find('.zong'))
 			var zongjia = parseInt(zong);
 
 			for(var i=0;i<goodslist.length;i++){
@@ -185,8 +183,7 @@ require(['config'],function(){
 				}
 			}
 			
-			time();
-
+			
 			biao.children().each(function(){
                 
                 var p = $(this).find('.duo_price').find('strong').text();
@@ -194,74 +191,13 @@ require(['config'],function(){
                 totalPrice = zongjia - p;  
                 
             });  
-            jinE.html(`商品总金额共计:<strong>${totalPrice.toFixed(2)}<strong/>`);
-            jinE_x.html(`商品总金额共计:<strong>${totalPrice.toFixed(2)}<strong/>`);
+            jinE.html(`商品总金额共计:<strong class="zong">${totalPrice.toFixed(2)}<strong/>`);
+            jinE_x.html(`商品总金额共计:<strong class="zong">${totalPrice.toFixed(2)}<strong/>`);
 
 			// 删除DOM节点
 			$(this).parent().parent().parent().parent().remove();
-
+			time();
 		})
 
-		// 利用cookie登录
-		var list = $('.list-l');
-
-		var cookies = document.cookie;
-		var res = cookies.split('=');
-
-		if(cookies && res[0] == 'username'){
-			var arr = cookies.split('; ');
-
-			arr.forEach(function(item){
-				var temp = item.split('=');
-				if(temp[0] === 'username'){
-					list.html (temp[1] + '，欢迎登录国家安全系统 <button class="tuichu">退出</button>');
-				}
-			});
-		}else{
-			list.html(`<li class="a">欢迎来到风尚购物</li>
-			<li class="deng"><a href="denglu.html">登录</a></li>
-			<li class="zhu"><a href="zhuce.html">注册</a></li>`);	
-		}
-
-		// 删除cookie
-		// 利用设置有效时间来达到删除的效果
-		list.on('click','.tuichu',function(){
-
-			var now = new Date('2017-5-9');
-
-			document.cookie = 'username=null;expires=' + now.toUTCString();
-			// setCookie('user','',now.toUTCString());
-
-			location.href = 'denglu.html';
-		})
-		var list = $('.list-l');
-
-		var cookies = document.cookie;
-
-		if(cookies){
-			var arr = cookies.split('; ');
-
-			arr.forEach(function(item){
-				var temp = item.split('=');
-				if(temp[0] === 'username'){
-					list.html (temp[1] + '，欢迎登录国家安全系统 <button class="tuichu">退出</button>');
-				}
-			});
-		}else{
-			list.html(`<li class="a">欢迎来到风尚购物</li>
-			<li class="deng"><a href="denglu.html">登录</a></li>
-			<li class="zhu"><a href="zhuce.html">注册</a></li>`);	
-		}
-
-		// 删除cookie
-		// 利用设置有效时间来达到删除的效果
-		list.on('click','.tuichu',function(){
-
-			var now = new Date('2017-5-9');
-
-			document.cookie = 'username=null;expires=' + now.toUTCString();
-
-			location.href = 'denglu.html';
-		})
 	})
 })
