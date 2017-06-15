903a7080dbdc4d590d024b45ea6d702be76964aa
shi12321 <767388689@qq.com>
2017-06-15 10:28:35 +0800

    登录系统

--- src/js/car.js
+++ src/js/car.js
@@ -198,6 +198,37 @@ console.log($(this).closest('.car_z_02').next().find('.zong'))
 			$(this).parent().parent().parent().parent().remove();
 			time();
 		})
+		// 头部登录系统
+		var list = $('.list-l');
+
+		var cookies = document.cookie;
+		
+		if(cookies){
+			var arr = cookies.split('; ');
+
+			arr.forEach(function(item){
+				var temp = item.split('=');console.log(temp[0])
+				if(temp[0] === 'username'){
+					list.html (temp[1] + '，欢迎登录国家安全系统 <button class="tuichu">退出</button>');
+				}
+				
+			});
+		}else{
+			list.html(`<li class="a">欢迎来到风尚购物</li>
+			<li class="deng"><a href="denglu.html">登录</a></li>
+			<li class="zhu"><a href="zhuce.html">注册</a></li>`);	
+		}
+
+		// 删除cookie
+		// 利用设置有效时间来达到删除的效果
+		list.on('click','.tuichu',function(){
+
+			var now = new Date('2017-5-9');
+
+			document.cookie = 'username=null;expires=' + now.toUTCString();
+
+			location.href = 'denglu.html';
+		})
 
 	})
 })
--- src/js/liebiao.js
+++ src/js/liebiao.js
@@ -72,6 +72,37 @@ require(['config'],function(){
 			})
 		}
 
+		// 头部登录系统
+		var list = $('.list-l');
+
+		var cookies = document.cookie;
+		
+		if(cookies){
+			var arr = cookies.split('; ');
+
+			arr.forEach(function(item){
+				var temp = item.split('=');console.log(temp[0])
+				if(temp[0] === 'username'){
+					list.html (temp[1] + '，欢迎登录国家安全系统 <span class="tuichu">退出</span>');
+				}
+				
+			});
+		}else{
+			list.html(`<li class="a">欢迎来到风尚购物</li>
+			<li class="deng"><a href="denglu.html">登录</a></li>
+			<li class="zhu"><a href="zhuce.html">注册</a></li>`);	
+		}
+
+		// 删除cookie
+		// 利用设置有效时间来达到删除的效果
+		list.on('click','.tuichu',function(){
+
+			var now = new Date('2017-5-9');
+
+			document.cookie = 'username=null;expires=' + now.toUTCString();
+
+			location.href = 'denglu.html';
+		})
 
 		
 	});
